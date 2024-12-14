893afc5fc54718557d7bd7aab90033257da0c0f6
AdieDev0 <devutella@gmail.com>
2024-12-14 11:34:43 +0800

    Testing

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// REST OBJECT
+const app = express();
+
+// REST API
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to e-commerce app",
+  });
+});
+
+
+//PORT 
+const PORT = 8080;
+
+// RUN LISTEN
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`);
+})
