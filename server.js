549891d4831bebd026091cb0d83b00bdc7b02b90
AdieDev0 <devutella@gmail.com>
2024-12-15 11:50:09 +0800

    Later

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 // CONFIGURE KEY
 dotenv.config();
@@ -15,7 +16,10 @@ connectDB();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+
+// ROUTES
+app.use("/api/v1/auth", authRoutes);
 
 // REST API
 app.get("/", (req, res) => {
