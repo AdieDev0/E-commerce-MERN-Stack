291d50bac96792ec862c8549e750c3949e4e0658
AdieDev0 <devutella@gmail.com>
2024-12-16 00:00:42 +0800

    Hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,44 @@
 // In authController.js
-const registerController = () => {
-    try{
-      const {name, email, password, phone, address} = req.body
+const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
 
-      // validation
-      
-    } catch(error){
-      console.logo(error)
-      res.status(500).send({
-        success:false,
-        message:'Error in Registration',
-        error
-      })
+    // validation
+    if (!name) {
+      return res.send({ error: "Name is required" });
     }
-  };
-  
-  export default registerController;
-  
+    if (!email) {
+      return res.send({ error: "Email is required" });
+    }
+    if (!password) {
+      return res.send({ error: "password is required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone is required" });
+    }
+    if (!address) {
+      return res.send({ error: "Address is required" });
+    }
+    // check user
+    const existingUser = await userModel.findOne({ email });
+    // existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already Register please login",
+      });
+    }
+
+    // register user
+    const hashedPassword = await hashedPassword(password);
+  } catch (error) {
+    console.logo(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+export default registerController;
