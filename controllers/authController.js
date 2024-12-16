90067e662d15e7f4a45557f0e4aad0211bb32302
AdieDev0 <devutella@gmail.com>
2024-12-16 23:30:53 +0800

    RENAME2

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,8 @@ export const registerController = async (req, res) => {
     // Validation
     if (!name) return res.status(400).send({ error: "Name is required" });
     if (!email) return res.status(400).send({ error: "Email is required" });
-    if (!password) return res.status(400).send({ error: "Password is required" });
+    if (!password)
+      return res.status(400).send({ error: "Password is required" });
     if (!phone) return res.status(400).send({ error: "Phone is required" });
     if (!address) return res.status(400).send({ error: "Address is required" });
 
@@ -48,7 +49,7 @@ export const registerController = async (req, res) => {
   }
 };
 
-// Login Controller
+// LOGIN CONTROLLER
 export const loginController = async (req, res) => {
   try {
     const { email, password } = req.body;
