622ab6c7332c1138188a76b4c8a0a037ae4cf5ea
AdieDev0 <devutella@gmail.com>
2024-12-18 02:01:15 +0800

    Pagenotfound

---
+++ frontend/pages/Pagenotfound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../src/components/layout";
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <h1>Pagenotfound</h1>
+    </Layout>
+  );
+};
+
+export default Pagenotfound;
