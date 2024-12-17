7dd0de1cb87d9d0a0f98e55554c0b3f0362aadcc
AdieDev0 <devutella@gmail.com>
2024-12-18 02:05:18 +0800

    move

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Layout from "./components/layout";
+import HomePages from "./pages/HomePages";
 const App = () => {
   return (
     <div>
-      <Layout />
+      <HomePages />
     </div>
   );
 };
