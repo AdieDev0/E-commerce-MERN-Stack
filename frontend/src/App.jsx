387ca9481aa04e1e38e08a4c543650066f4829e7
AdieDev0 <devutella@gmail.com>
2024-12-18 02:15:27 +0800

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePages from "./pages/HomePages";
 import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Policy from "./pages/Policy.jsx";
+import Pagenotfound from "./pages/Pagenotfound.jsx";
 
 
 const App = () => {
@@ -10,6 +13,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePages />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<Pagenotfound />} />
+
+
 
       </Routes>
     </Router>
