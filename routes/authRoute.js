2c2dfaf994bf2bfeed64ef68794ea236ccdb3744
AdieDev0 <devutella@gmail.com>
2024-12-17 23:56:39 +0800

    requireSignIn

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,7 @@ import {
   loginController,
   testController,
 } from "../controllers/authController.js";
-
+import { requireSignIn } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // Register Route
@@ -14,6 +14,6 @@ router.post("/register", registerController);
 router.post("/login", loginController);
 
 // TEST ROUTES
-router.get("/test", testController);
+router.get("/test", requireSignIn, testController);
 
 export default router;
