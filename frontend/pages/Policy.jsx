298469cb94ffd3cadc740eb124c3b3cd222a03aa
AdieDev0 <devutella@gmail.com>
2024-12-18 01:58:22 +0800

    Update

--- frontend/pages/Policy.jsx
+++ frontend/pages/Policy.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Layout from "../src/components/layout";
 const Policy = () => {
-  return <Layout>Polocy</Layout>;
+  return (
+    <Layout>
+      <h1>Polocy</h1>
+    </Layout>
+  );
 };
 
 export default Policy;
