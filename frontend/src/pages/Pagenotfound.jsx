ae95afdcb2520d919795099cc2163fa0f4b5f026
AdieDev0 <devutella@gmail.com>
2024-12-18 12:33:17 +0800

    Update Layout and Page Not Found

--- frontend/src/pages/Pagenotfound.jsx
+++ frontend/src/pages/Pagenotfound.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Lottie from "lottie-react"; // Default import
 import Layout from "../components/Layout.jsx";
-const Pagenotfound = () => {
+import notFound from "../assets/notFound.json";
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // Navigate back to the previous page
+  };
+
   return (
-    <Layout>
-      <h1>Pagenotfound</h1>
-    </Layout>
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-screen text-center">
+        {/* Render Lottie Animation */}
+        <Lottie animationData={notFound} loop className="w-64 h-64 mb-4" />
+        <h1 className="text-2xl font-bold font-Outfit mb-5 ">Oops! Page Not Found</h1>
+        <button
+          onClick={handleGoBack}
+          className="px-4 py-2 font-Outfit bg-violet-700 text-white rounded hover:bg-violet-600 transition"
+        >
+          Go Back
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default Pagenotfound;
+export default PageNotFound;
