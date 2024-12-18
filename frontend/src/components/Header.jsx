0cd670a6d612b66f1f5b7de6a4e415c10232eb10
AdieDev0 <devutella@gmail.com>
2024-12-18 10:13:48 +0800

    Update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="bg-slate-300 py-7 px-14">
+      <div className="flex justify-between">
+        <div>
+          <p className="cursor-pointer">Logo</p>
+        </div>
+        <div>
+          <ul className="flex gap-5">
+            <li className="cursor-pointer">
+              <NavLink to="/" />
+              Home
+            </li>
+            <li className="cursor-pointer">
+              <NavLink to="/" />
+              Register
+            </li>
+            <li className="cursor-pointer">
+              <NavLink to="/" />
+              Login
+            </li>
+            <li className="cursor-pointer">
+              <NavLink to="/" />
+              Cart(0)
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
